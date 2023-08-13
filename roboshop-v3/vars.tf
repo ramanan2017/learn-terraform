5e6a576b31a2d4c1c61e9b8552d1f1b2ade04976
Ram <ratram2010@gmail.com>
2023-08-13 21:19:28 +0100

    roboshop-terraform v 3.0

--- roboshop-v3/vars.tf
+++ roboshop-v3/vars.tf
@@ -1,19 +1,55 @@
-variable "security_group" {
-  default = ["sg-0091ad00710b5ec85"]
+variable "security_groups" {
+  default = [ "sg-0dee954b08055e577" ]
 }
 
 variable "zone_id" {
-  default = "Z02461421H4DZFKN2OZ9Y"
+  default = "Z0021413JFIQEJP9ZO9Z"
 }
 
 variable "components" {
   default = {
     frontend = {
-      name          = "frontend"
+      name = "frontend"
       instance_type = "t3.micro"
     }
     cart = {
-      name          = "cart"
+      name = "cart"
+      instance_type = "t3.micro"
+    }
+    mongodb = {
+      name = "mongodb"
+      instance_type = "t3.micro"
+    }
+    catalogue = {
+      name = "catalogue"
+      instance_type = "t3.micro"
+    }
+    user = {
+      name = "user"
+      instance_type = "t3.micro"
+    }
+    redis = {
+      name = "redis"
+      instance_type = "t3.micro"
+    }
+    mysql = {
+      name = "mysql"
+      instance_type = "t3.micro"
+    }
+    payment = {
+      name = "payment"
+      instance_type = "t3.micro"
+    }
+    shipping = {
+      name = "shipping"
+      instance_type = "t3.micro"
+    }
+    rabbitmq = {
+      name = "rabbitmq"
+      instance_type = "t3.micro"
+    }
+    dispatch = {
+      name = "dispatch"
       instance_type = "t3.micro"
     }
   }
